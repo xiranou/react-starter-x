5e0c3e808da41d7acad64c50ebbd154af65523a0
xiranou <xiranou@gmail.com>
2017-04-21 15:13:19 -0400

    initial SSR shell

--- app/client/index.js
+++ app/client/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from 'app/shared/Routes';
-
 
 render((
   <BrowserRouter>
