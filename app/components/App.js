f67101ec34ab19305eb8147bb9a1b9b1d661eeef
xiranou <xiranou@gmail.com>
2017-04-24 14:57:42 -0400

    remove test code

--- app/components/App.js
+++ app/components/App.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      stuff: null
-    };
-  }
-
-  async getChange() {
-    const stuff = await change();
-
-    this.setState({
-      stuff
-    });
-  }
-
-  componentWillMount() {
-    this.getChange();
-  }
-
   render() {
     return (
       <div>
         <h1>I'm App!</h1>
-        {
-          this.state.stuff ? (
-            <span>{this.state.stuff}</span>
-          ) : null
-        }
       </div>
     )
   }
-}
-
-function change() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Hey!');
-    }, 500);
-  });
 }
